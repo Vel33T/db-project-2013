f00945e69be36340a76ee38ca1369482db58b584
petyob@gmail.com <petyob@gmail.com@d196510b-2250-1cfb-ad82-6e07a61a5137>
2013-07-23 13:27:55 +0000

    task6 - total report

--- trunk/Supermarkets.Task6.TotalReport/Program.cs
+++ trunk/Supermarkets.Task6.TotalReport/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace Supermarkets.Task6.TotalReport
 {
@@ -10,6 +12,46 @@ namespace Supermarkets.Task6.TotalReport
     {
         static void Main(string[] args)
         {
+            SQLiteConnection dbCon = new SQLiteConnection(@"..\..\taxes.sqlite");
+
+            using (dbCon)
+            {
+                dbCon.Open();
+
+                //                var create = new SQLiteCommand(
+                //    @"CREATE TABLE IF NOT EXISTS ProductsTax
+                //(
+                //    ProductName NVARCHAR(64),
+                //    Tax INTEGER
+                //)",
+                //    dbCon);
+                //                create.ExecuteNonQuery();
+
+            }
+
+        }
+
+        static void ListAllProductsTax(this SQLiteConnection conn)
+        {
+            var command = conn.CreateCommand();
+            command.CommandText = "SELECT * FROM ProductsTax";
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    // Console.WriteLine("{0}", reader.ShowReaderRow());
+                }
+            }
+        }
+
+        static void AddProductTax(this SQLiteConnection conn, string productName, int tax)
+        {
+            var command = conn.CreateCommand();
+            command.CommandText = "INSERT INTO ProductsTax(ProductName, Tax) VALUES(@productName, @tax)";
+            command.Parameters.Add(new SQLiteParameter("@ProductName", productName));
+            command.Parameters.Add(new SQLiteParameter("@title", tax));
+            command.ExecuteNonQuery();
         }
     }
 }
