55582bdd06940bdbc009bef2af2d65136cdb60ae
velko.nikolov <velko.nikolov@gmail.com>
2013-07-23 15:32:15 +0000

    integrating

--- Supermarkets/Program.cs
+++ Supermarkets/Program.cs
@@ -43,31 +43,62 @@ namespace Supermarkets
                 }
             }
 
-            if (Ask("Run Task 2?"))
-            {
-                var file = @"output\report-aggregate-sales.pdf";
 
-                using (var sqlserver = new SupermarketsEntities())
+            using (var sqlserver = new SupermarketsEntities())
+            {
+                if (Ask("Run Task 2?"))
                 {
+                    var file = @"output\report-aggregate-sales.pdf";
+
+
                     Directory.CreateDirectory("output");
                     Supermarkets.Task2.PDF.PdfSalesReport.GeneratePdfReport(sqlserver, file);
                 }
 
                 Console.WriteLine("PDF aggregate sales report in " + file);
 
-            }
 
-            if (Ask("Run Task 3?"))
-            {
-                var file = @"output\report-vendor-sales.xml";
-
-                using (var sqlserver = new SupermarketsEntities())
+                if (Ask("Run Task 3?"))
                 {
+                    var file = @"output\report-vendor-sales.xml";
+
+
                     Directory.CreateDirectory("output");
                     Supermarkets.Task3.XML.GenerateXMLFile.GenerateAggregateReport(sqlserver, file);
+
+                    Console.WriteLine("XML vendor sales report in " + file);
+
                 }
 
-                Console.WriteLine("XML vendor sales report in " + file);
+                if (Ask("Run task 4?"))
+                {
+                    var file = @"output\task-4.xml";
+
+                    Supermarkets.Task4.MongoDB.InsertIntoMongoDB.GenerateMongoDBProductReport();
+
+                    Console.WriteLine("Task 4 report in " + file);
+
+                }
+
+                if (Ask("Run task 5?"))
+                {
+                    var file = @"output\vendor-expenses-report.xml";
+
+                    Supermarkets.Task5.VendorExpencesXML.GenerateVendorExpenses.WriteVendorExpensesReport(file);
+
+                    Console.WriteLine("Task 5 report in " + file);
+
+                }
+
+                if (Ask("Run Task 6?"))
+                {
+                    var file = @"output\final-report.xlsx";
+
+                    // Supermarkets.Task6.TotalReport.ExcelWriter.GenerateExcel(sqlserver, file);
+
+                    Console.WriteLine("Final report in " + file);
+
+                }
 
             }
 
