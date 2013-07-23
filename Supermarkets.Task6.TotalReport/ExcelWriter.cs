9ba8ff8124d8ca4d2bf2dacc22c9f6b80c34c198
petyob <petyob@gmail.com>
2013-07-23 15:29:00 +0000

    fix: append "xml" property in connection string

--- Supermarkets.Task6.TotalReport/ExcelWriter.cs
+++ Supermarkets.Task6.TotalReport/ExcelWriter.cs
@@ -2,12 +2,13 @@
 using System.Data.OleDb;
 using System.IO;
 using System.Linq;
+using Supermarkets.SQLite.EntityFramework;
 
 namespace Supermarkets.Task6.TotalReport
 {
     class ExcelWriter
     {
-        public static void GenerateExcel(IQueryable<VendorsTotalReport> items, string pathToExcel)
+        public static void GenerateExcel(IQueryable<VendorFinancialResult> items, string pathToExcel)
         {
             if (File.Exists(pathToExcel))
             {
@@ -16,32 +17,33 @@ namespace Supermarkets.Task6.TotalReport
 
             var connectionString = new OleDbConnectionStringBuilder();
             connectionString.Provider = "Microsoft.ACE.OLEDB.12.0";
-            connectionString.Add("Extended Properties", "Excel 12.0");
+            connectionString.Add("Extended Properties", "Excel 12.0 xml");
+            connectionString.DataSource = pathToExcel;
 
             InsertIntoExcelDoc(connectionString, items);
         }
 
-        public static void InsertIntoExcelDoc(OleDbConnectionStringBuilder connectionString, IQueryable<VendorsTotalReport> items)
+        public static void InsertIntoExcelDoc(OleDbConnectionStringBuilder connectionString, IQueryable<VendorFinancialResult> items)
         {
             using (OleDbConnection connection = new OleDbConnection(connectionString.ToString()))
             {
                 connection.Open();
 
-                OleDbCommand command = new OleDbCommand(@"CREATE TABLE [Sheet33] 
-([Vendor Name] string, [Incomes] decimal, [Expenses] decimal, [Taxes] decimal, [Financial Result] decimal)", dbCon);
+                OleDbCommand command = new OleDbCommand(@"CREATE TABLE [Sheet1] 
+([Vendor Name] string, [Incomes] decimal, [Expenses] decimal, [Taxes] decimal, [Financial Result] decimal)", connection);
                 command.ExecuteScalar();
 
                 foreach (var vendor in items)
                 {
                     OleDbCommand insertQuery = new OleDbCommand(
-                        @"INSERT INTO [Sheet33$] ([Vendor], [Incomes], [Expenses], [Taxes], [Financial Result]) 
+                        @"INSERT INTO [Sheet1$] ([Vendor], [Incomes], [Expenses], [Taxes], [Financial Result]) 
                 VALUES (@vendorName, @incomes, @expenses, @taxes, @result)", connection);
 
                     insertQuery.Parameters.AddWithValue("@vendorName", vendor.VendorName);
-                    insertQuery.Parameters.AddWithValue("@income", vendor.Incomes);
-                    insertQuery.Parameters.AddWithValue("@expenses", vendor.Expences);
-                    insertQuery.Parameters.AddWithValue("@taxes", vendor.Taxes);
-                    insertQuery.Parameters.AddWithValue("@result", vendor.Result);
+                    insertQuery.Parameters.AddWithValue("@income", vendor.Income);
+                    insertQuery.Parameters.AddWithValue("@expenses", vendor.Expenses);
+                    insertQuery.Parameters.AddWithValue("@taxes", vendor.Tax);
+                    insertQuery.Parameters.AddWithValue("@result", vendor.FinancialResult);
 
                     insertQuery.ExecuteNonQuery();
                 }
